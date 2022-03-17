b21e41961dfbe4098f4ebca1b3763aedcd425e11
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-17 21:43:12 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -65,7 +65,7 @@ const Auth = Router()
             res.status(200).redirect("/");
             req.dashboardConfig.mode[userData.infos.id] = "dark"
         } else {
-            res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${req.client?.user?.id}&scope=identify%20guilds&response_type=code&redirect_uri=${encodeURIComponent(`${req.dashboardConfig.baseUrl}${req.dashboardConfig.noPortIncallbackUrl ? '' : ':' + req.dashboardConfig.port}/auth/login`)}`);
+            res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${req.client?.user?.id}&scope=identify%20email%20guilds&response_type=code&redirect_uri=${encodeURIComponent(`${req.dashboardConfig.baseUrl}${req.dashboardConfig.noPortIncallbackUrl ? '' : ':' + req.dashboardConfig.port}/auth/login`)}`);
         }
     })
     .get("/logout", [CheckAuth], function (req, res) {
