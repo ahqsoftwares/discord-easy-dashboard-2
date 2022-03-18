b8a182dad2b7223db29a20e5edcb7373e7068f22
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-03-18 21:35:25 +0530

    possible fix

--- routes/selector.js
+++ routes/selector.js
@@ -44,32 +44,7 @@ const Selector = Router().get("/", CheckAuth, async (req, res) => {
     }
     req.session.user.data.filter = true;
     
-    return await res.render(
-        file,
-        {
-            bot: req.client,
-            user: req.user,
-            guilds: req.user.guilds.sort((a, b) =>
-                a.name < b.name ? -1 : Number(a.name > b.name)
-            ),
-            is_logged: Boolean(req.session.user),
-            Perms: Permissions,
-            path: req.path,
-            baseUrl: req.dashboardConfig.baseUrl,
-            port: req.dashboardConfig.port,
-            dashboardDetails: req.dashboardDetails,
-            dashboardConfig: req.dashboardConfig,
-            filter_server: true,
-            email: Boolean(req.session.user.data.email),
-        },
-        (err, html) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return console.error(err);
-            }
-            res.status(200).send(html);
-        }
-    );
+    return await res.redirect("/selector");
 })
 .get("/filter_false", [CheckAuth], async (req, res) => {
     let file = req.dashboardConfig.theme["selector"] || "selector.ejs";
@@ -79,32 +54,7 @@ const Selector = Router().get("/", CheckAuth, async (req, res) => {
     }
     req.session.user.data.filter = false;
 
-    return await res.render(
-        file,
-        {
-            bot: req.client,
-            user: req.user,
-            guilds: req.user.guilds.sort((a, b) =>
-                a.name < b.name ? -1 : Number(a.name > b.name)
-            ),
-            is_logged: Boolean(req.session.user),
-            Perms: Permissions,
-            path: req.path,
-            baseUrl: req.dashboardConfig.baseUrl,
-            port: req.dashboardConfig.port,
-            dashboardDetails: req.dashboardDetails,
-            dashboardConfig: req.dashboardConfig,
-            filter_server: false,
-            email: Boolean(req.session.user.data.email),
-        },
-        (err, html) => {
-            if (err) {
-                res.status(500).send(err.message);
-                return console.error(err);
-            }
-            res.status(200).send(html);
-        }
-    );
+    return await res.redirect("/selector");
 });
 
 module.exports.Router = Selector;
